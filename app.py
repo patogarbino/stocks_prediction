d086b8f8c184187b630e542f17b6c8a61e1a4689
vanesa camano <vanesasoledad90@gmail.com>
2022-03-11 19:39:35 +0100

    streamlit

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 import streamlit as st
 import requests
+from streamlit_lottie import st_lottie
 
-st.title('Stock market performance prediction')
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# ---- LOAD ASSETS ----
+
+
+
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.title("Stock market performance prediction")
+    st.write(
+        "Choose a tick and predict the performance of a share against the market performance"
+    )
+
 
 #st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 
+# ---- PREDICTION ----
+with st.container():
+    st.write("---")
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.header("What I do")
+        st.write("##")
+
+    st.markdown('Choose a tick to predict the performance of the share')
+    tick = st.input(label='TICK')
+    button=st.button('Predict')
 
 
-st.markdown('Choose a tick to predict the performance of the share')
+    with right_column:
+        st_lottie(lottie_coding, height=300, key="coding")
 
 
-tick = st.date_input(label='TICK')
 
-button=st.button('Predict')
 
 
 st.write('Ticks availables')
