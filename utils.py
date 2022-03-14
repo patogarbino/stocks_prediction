33d77d01d3eec1282c56f219b5d5224308a14619
Patricio Garbino <pato.garbino@gmail.com>
2022-03-14 13:49:05 -0300

    function clean added

--- utils.py
+++ utils.py
@@ -1,37 +1,63 @@
 import requests
 import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
 
-ticker = 'AAPL'
-def get_data(ticker):
+def get_data(ticker, function):
+
+    url = 'https://www.alphavantage.co/query'
+    params = {'function': function,
+          'symbol': ticker,
+          'apikey':'9Q6MRAOS3OPYVK3R'
+    }
+
+    return requests.get(url,params = params).json()
+
+def merge_data (ticker):
 
     df = pd.DataFrame()
 
-    for function in ['INCOME_STATEMENT','BALANCE_SHEET','CASH_FLOW','EARNINGS']:
-        try:
-            url = 'https://www.alphavantage.co/query'
-            params = {'function': function,
-                  'symbol': ticker,
-                  'apikey':'C2FFBBIRN1B0SD6Z'
-            }
+    r = get_data(ticker,'INCOME_STATEMENT')
+    response = pd.DataFrame(r['quarterlyReports'])
+    response.drop(columns = 'reportedCurrency',inplace = True)
+    df = pd.concat([df,response], axis= 1)
 
-            r = requests.get(url,params = params).json()
+    r = get_data(ticker,'BALANCE_SHEET')
+    response = pd.DataFrame(r['quarterlyReports'])
+    response.drop(columns = 'reportedCurrency',inplace = True)
+    df = df.merge(response, on = 'fiscalDateEnding')
+
+    r = get_data(ticker,'CASH_FLOW')
+    response = pd.DataFrame(r['quarterlyReports'])
+    response.drop(columns = 'reportedCurrency',inplace = True)
+    df = df.merge(response, on = 'fiscalDateEnding')
+
+    r = get_data(ticker,'EARNINGS')
+    response = pd.DataFrame(r['quarterlyEarnings'])
+    df = df.merge(response, on = 'fiscalDateEnding')
+
+    return df.head(1)
 
 
-            if function in ['INCOME_STATEMENT','BALANCE_SHEET','CASH_FLOW']:
+def  clean_data(df):
+    cols_to_drop = ['proceedsFromSaleOfTreasuryStock','paymentsForRepurchaseOfPreferredStock','dividendPayoutPreferredStock',
+                'proceedsFromOperatingActivities','proceedsFromIssuanceOfPreferredStock','changeInExchangeRate','investmentIncomeNet',
+                'deferredRevenue','depreciation','capitalLeaseObligations','treasuryStock','proceedsFromIssuanceOfCommonStock',
+                'longTermDebtNoncurrent','researchAndDevelopment','paymentsForRepurchaseOfCommonStock','investments',
+                'accumulatedDepreciationAmortizationPPE','proceedsFromIssuanceOfLongTermDebtAndCapitalSecuritiesNet',
+                'proceedsFromRepaymentsOfShortTermDebt','changeInInventory','paymentsForRepurchaseOfEquity','currentDebt',
+                'interestIncome','shortTermInvestments','dividendPayoutCommonStock','longTermInvestments','dividendPayout',
+                'currentAccountsPayable','currentNetReceivables','inventory','paymentsForOperatingActivities',
+                'otherNonOperatingIncome','goodwill','interestAndDebtExpense','changeInReceivables','currentLongTermDebt',
+                'nonInterestIncome','changeInOperatingLiabilities','intangibleAssetsExcludingGoodwill','shortTermDebt',
+                'changeInOperatingAssets','otherNonCurrentLiabilities','intangibleAssets','longTermDebt','netInterestIncome',
+                'fiscalDateEnding','reportedDate']
 
-                response = pd.DataFrame(r['quarterlyReports'])
-                response.drop(columns = 'reportedCurrency',inplace = True)
-                df = pd.concat([df,response], axis= 1)
+    preproc_data = pd.read_csv('data/preproc_data.csv')
 
+    df.drop(columns = cols_to_drop,inplace = True)
+    pipeline = make_pipeline(SimpleImputer(strategy='median'))
+    pipeline.fit(preproc_data)
+    df  = pipeline.transform(df)
 
-            else:
-                print('Earnings')
-
-                response = pd.DataFrame(r['quarterlyEarnings'])
-                df = df.merge(response, on = 'fiscalDateEnding')
-                print('Earnings_2')
-            return df
-
-        except:
-            break
-    return 'No se pudo obtener la información necesaria'
+    return df
